2edac16d5229824c72786048f790f3a5773ae23c
Junzhe Zhao <89234341+JunzheCS2@users.noreply.github.com>
2021-09-15 02:13:51 +0000

    commit

--- Assignment 8.3/main.cpp
+++ Assignment 8.3/main.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
 using namespace std;
 
+int findMin(int [], int, int);
+
 int main() {
   const int N = 10;
-  int numbers[N] = {};
+  int numbers[N] = {25,10,15,30,35,40,45,55};
+
+  for(int i =0;i<N-1;i++)
+  {
+    int minidx;
+    minidx = findMin(numbers, N, i);
+    swap(numbers[i], numbers[minidx]);
+  }
 }
+
+  int findMin(int numbers[], int N, int ith)
+  {
+    int min,minidx;
+    min = numbers[ith];
+    for(int i =ith;i<N;i++)
+    {
+      if(min > numbers[i])
+      {
+        min = numbers[i];
+        minidx = i;
+      }
+    }
+    return i;
+  }
+
+  int printArray(int [], int x)
+  {
+    for(int i =0;i< x;i++)
+    {
+      cout << 
+    }
+  }
