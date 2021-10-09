fb3e5d565f05b79509c999004b967ebfb5bf2e9a
Junzhe Zhao <89234341+JunzheCS2@users.noreply.github.com>
2021-10-09 04:20:04 +0000

    update 8-3

--- assignment8-3.cpp
+++ assignment8-3.cpp
@@ -2,47 +2,46 @@
 using namespace std;
 
 int findMin(int [], int, int);
+void printArray(int [], int);
 
-int main() {
-  const int N = 10;
-  int numbers[N] = {25,10,15,30,35,40,45,55};
+int main()
+{
+	const int N = 10;
+	int numbers[N] = {25, 10, 15, 30, 35, 40, 45, 55,20,50};
+	int minidx;
 
-  for(int i =0;i<N-1;i++)
-  {
-    int minidx;
-    minidx = findMin(numbers, N, i);
-    swap(numbers[i], numbers[minidx]);
-  }
+	printArray(numbers, N);
+
+	for(int i=0;i<N-1;i++)
+    {
+        minidx= findMin(numbers, N, i); 
+		swap(numbers[i], numbers[minidx]);
+	}
+
+	printArray(numbers, N);
+
 }
 
-  int findMin(int numbers[], int N, int ith)
-  {
-    int min,minidx;
-    min = numbers[ith];
-    
-    // add this line
-    // minidx = ith;
-    
-    for(int i =ith;i<N;i++)
-    {
-      if(min > numbers[i])
-      {
-        min = numbers[i];
-        minidx = i;
-      }
-    }
-    return ith;
-    
-    // You returned ith. I don't think it will work fine. The expected return value is the index of the min value.
-    // Fix this logical error and let me know .
-    
-  }
+int findMin(int numbers[], int N, int ith)
+{
+	int min, minidx, i;
+	min = numbers[ith];
+    for(i=ith ;i<N;i++)
+	{
+		if ( min > numbers[i] )
+		{
+			min = numbers[i];
+			minidx = i;
+		}
+	}
+	return minidx; 
+}
 
-  int printArray(int a[], int x)
-  {
-    for(int i =0;i< x;i++)
-    {
-      cout << a[i] << "\t";
-    }
-    cout << "\n";
+void printArray(int a[], int x)
+{
+	for(int i = 0; i < x; i++){
+    cout << a[i] << "\t";
   }
+  cout << endl;
+  
+}
