fb22efdff7e451949d783a7168f3835299753885
Junzhe Zhao <89234341+JunzheCS2@users.noreply.github.com>
2021-10-20 04:00:02 +0000

    commit 9-5.cpp

---
+++ assignment9-5.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+  int num;
+  struct Node* next;
+};
+
+
+int main()
+{
+	Node *new_node, *head, *ptr;
+	head = nullptr;
+
+	for(int i=0; i<5;i++)
+	{
+		new_node = new Node;
+		cout << "Enter your num : " ;
+		cin >> new_node->num;
+		new_node->next = nullptr;
+
+		if ( head == nullptr)
+		{
+			head = new_node;
+		}
+		else
+		{
+			ptr = head ;
+			while( ptr->next != nullptr)
+			{
+				ptr = ptr->next;
+			}
+			ptr->next = new_node;
+		}
+	}
+    
+    int i = 1;
+    ptr = head;
+    
+	while(ptr != nullptr)
+    {
+		cout << i++ << " node's value " << ptr->num << endl;        
+		ptr = ptr->next;
+    }
+
+
+}
