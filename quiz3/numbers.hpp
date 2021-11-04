afa00215c601ad3b6bc26051969ebd3e46886452
Junzhe Zhao <89234341+JunzheCS2@users.noreply.github.com>
2021-11-04 23:33:40 +0000

    commit numbers.hpp

--- quiz3/numbers.hpp
+++ quiz3/numbers.hpp
@@ -6,5 +6,19 @@ class NUMBERS
   private:
     int ID;
     vector<int> numbers;
-    
-}
+  public:
+    numbers();
+    numbers(int,int);
+    int getID() const;
+    int getSize() const;
+    int getElm(int i) const;
+    int getMax() const;
+    int getMin() const;
+    int getSum() const;
+    void deleteElm(int d);
+    void addElm(int a);
+    void printAll() const;
+
+};
+
+#endif
