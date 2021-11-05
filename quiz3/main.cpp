ed23ea2cf4598b12dd4f55e938c75e6e42a15113
Junzhe Zhao <89234341+JunzheCS2@users.noreply.github.com>
2021-11-05 00:12:34 +0000

    commit main.cpp

---
+++ quiz3/main.cpp
@@ -0,0 +1,34 @@
+#include "numbers.cpp"
+#include <cstdlib>
+#include<ctime>
+#include <iostream>
+using namespace std;
+
+int findMax(Numbers[], int);
+void deDup(Numbers &, Numbers &);
+
+int main()
+{
+  const int N =5;
+  srand(time(0));
+  Numbers numberset[] = {Numbers(1,5), Numbers(2,5),Numbers(3,10),Numbers(4,5),Numbers(5,7)};
+
+  for(int i = 0;i<N;i++)
+  {
+    cout << " Number Set ID " << numberset.getID() << "\t\n";
+    numberset[i].printAll();
+  }
+
+  int id = findMax(numberset,N);
+
+  cout << " the set ID that has max difference is " << id << "\t";
+  cout << " The diff is " << numberset[id].getMax() - numberset[id].getMin() << endl;
+
+  deDup(numberset[2], numberset[3]);
+  cout << " ID 3 - ID 4\n";
+  numberset[2].printALl();
+
+  deDup(numberset[0],numberset[1]);
+  cout << "ID 1 - ID2\n";
+  numberset[0].printAll();
+}
