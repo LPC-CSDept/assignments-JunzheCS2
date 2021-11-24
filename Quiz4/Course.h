de4ac9bd0018e1072a18f851edc3710be95ab493
Junzhe Zhao <89234341+JunzheCS2@users.noreply.github.com>
2021-11-24 05:18:50 +0000

    commit course.hpp

--- Quiz4/Course.h
+++ Quiz4/Course.h
@@ -8,5 +8,26 @@ using namespace std;
 
 class Course
 {
+  private:
+    string cname;
+    int credits;
+    string semester;
+    vector<Student> student;
+    static int NUM_COURSES;
+  public:
+    Course();
+    Course(string, int, string, vector<Student>);
 
-}
+    string getCname();
+    int getCredits();
+    string getSemester();
+    vector<Student> getStudent();
+    static int getNumCourses();
+
+    void setCourse(string, int, string, vector<Student>);
+    void addStudent(Student &s);
+
+    friend ifstream &operator>>(ifstream &, Course &);
+    friend ostream &operator>>(ostream &, Course &);
+
+};
