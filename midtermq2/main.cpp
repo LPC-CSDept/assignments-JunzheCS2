c4e8649f33ac791bc4886043a0ca6eacc62a078f
Junzhe Zhao <89234341+JunzheCS2@users.noreply.github.com>
2021-12-01 06:47:13 +0000

    update

--- midtermq2/main.cpp
+++ midtermq2/main.cpp
@@ -25,5 +25,58 @@ class Student
       this->score3 = score3;
     }
 
+    void setId(int id)
+    {
+      Student::id = id;
+    }
+
+    void setName(const string &name)
+    {
+      Student::name = name;
+    }
+
+    void setScore1(int score1)
+    {
+      Student::score1 = score1;
+    }
+
+    void setScore2(int score2)
+    {
+      Student::score2 = score2;
+    }
+
+    void setScore3(int score3)
+    {
+      Student::score3 = score3;
+    }
+
+    int getId() const
+    {
+      return id;
+    }
+
+    const string &getName() const
+    {
+      return name;
+    }
+
+    int getScore1() const
+    {
+      return score1;
+    }
+
+    int getScore2() const
+    {
+      return score2;
+    }
+
+    int getScore3() const
+    {
+      return score3;
+    }
+
+
+    
+
 };
 #endif
