cebae760b8134ed5a6525891fb043032ad846ec7
Junzhe Zhao <89234341+JunzheCS2@users.noreply.github.com>
2021-12-11 19:27:43 +0000

    update 13.4

--- 13.4/main.cpp
+++ 13.4/main.cpp
@@ -1,35 +1,29 @@
 #include "stack.hpp"
 #include <iostream>
+#include <vector>
 #include <cstring>
 using namespace std;
 
-int main()
+int main() 
 {
-	int numbers[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
-	Stack<int>	s(10);
+  int size = 20;
+  int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12,13,14,15,16,17,18,19,20};
 
-	for( int i=0; i < 10; i++)
-		s.push(numbers[i]);
-	s.printAll();
-	for( int i=0; i < 10; i++)
-		numbers[i] = s.pop(); 
-		
-	for( int i=0; i < 10; i++)
-		cout << numbers[i] << endl;
+  stack<int> array(20); // not sure why stack is not applicable.
 
+  for(int i=0;i<size;i++)
+  {
+    array.push(numbers[i]);
+  }
 
+  array.printAll();
 
-	double dvalue[] = {12.34, 45.12, 22, 11.0, 22.0};
-	Stack<double>	stack_double(10);
-	for( int i=0; i < 5; i++)
-		stack_double.push(dvalue[i]);
-	stack_double.printAll();
-
-
-	char cvalue[] = "Programming"; 
-	Stack<char>	stack_char(strlen(cvalue));
-	for( int i=0; i < strlen(cvalue); i++)
-		stack_char.push(cvalue[i]);
-	stack_char.printAll();
-
-}
+  for(int i=0;i<size;i++)
+  {
+    numbers[i] = array.pop();
+  }
+  for(int i=0;i<size;i++)
+  {
+    cout << numbers[i] << endl; // after pop.
+  }
+} 
---
+++ 13.4/stack.cpp
@@ -0,0 +1,52 @@
+#include "stack.hpp"
+using namespace std;
+stack::stack()
+{
+  pool.reserve(10);
+}
+
+stack::stack(int n)
+{
+  pool.reserve(n);
+}
+
+void stack::clear() //first time compile this could not work because this should be a void
+{
+  pool.clear(); //clear all the data
+}
+
+void stack::isEmpty()//check if the stack is isEmpty
+{
+  pool.empty();
+}
+
+void stack::push(int el)
+{
+  pool.push_back(el); //push el on top of the stack
+}
+
+int stack::pop()
+{
+  int n = pool.back();
+  pool.pop_back();
+  return n; // pop means that remove the top value and in the () of pop, there should not be any value in it.
+} 
+
+int stack::topEL()//put the elements on top
+{
+  return pool.back();
+}
+
+int stack::getSize() //get pool size
+{
+  return pool.size();
+}
+
+void stack::printAll() // print value
+{
+  cout << " values are : " << endl;
+  for(int i=0;i<pool.size();i++)
+  {
+    cout << pool[i] << endl;
+  }
+}
