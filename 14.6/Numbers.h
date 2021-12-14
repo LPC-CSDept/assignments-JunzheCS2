56285063205c27f95c7a2179c0723555a30c5a7e
Junzhe Zhao <89234341+JunzheCS2@users.noreply.github.com>
2021-12-14 00:43:58 +0000

    update 14.6

--- 14.6/Numbers.h
+++ 14.6/Numbers.h
@@ -12,15 +12,101 @@ class Numbers
   int *numbers;
   public:
   Numbers();
-  Numbers(int);
-  Numbers(const Numbers &);
-  ~Numbers();
-  int operator>(const Numbers &);
-  Numbers operator=(const Numbers &);
-  Numbers operator+(const Numbers &);
-  friend void printNumbers(Numbers);
-  friend ostream &operator << (ostream &, const Numbers &);
-  friend istream &operator >> (istream &, const Numbers &);
+  Numbers(int n)
+  {
+    size = n;
+    numbers = new int[n];
+    for(int i=0;i<size;i++)
+    {
+      numbers[i] = rand()%99;
+    }
+  };
+
+  Numbers(const Numbers &n)
+  {
+    size = n.size;
+    numbers = new int[size];
+    for(int i=0;i<size;i++)
+    {
+      numbers[i] = n.numbers[i];
+    }
+  };
+
+  ~Numbers()
+  {
+    delete [] numbers;
+  };
+
+  int operator>(const Numbers &n)
+  {
+    int num1 = 0;
+    int num2 = 0;
+    for(int i=0;i<size;i++)
+    {
+      num1 = num1 + numbers[i];
+    }
+    for(int i=0;i<size;i++)
+    {
+      num2 = num2 + n.numbers[i];
+    }
+    
+    if(num1 > num2)
+    {
+      return 1;
+    }
+    else
+      return 0;
+  };
+
+  Numbers operator=(const Numbers &n)
+  {
+    this -> size = n.size;
+    for(int i=0;i<size;i++)
+    {
+      this -> numbers[i] = n.numbers[i];
+    }
+    return n;
+  };
+
+  Numbers operator+(const Numbers &n)
+  {
+    int num1 = 0;
+    int num2 = 0;
+    for(int i=0;i<size;i++)
+    {
+      num1 = num1 + numbers[i];
+    }
+    for(int i=0;i<size;i++)
+    {
+      num2 = num2 + n.numbers[i];
+    }
+
+    return num1 + num2;
+    
+  };
+  friend void printNumbers(Numbers n)
+  {
+    for (int i=0; i<n.size; i++)
+    {
+      cout << n.numbers[i] << endl;
+    }
+  }
+  friend ostream &operator << (ostream &s, const Numbers &n)
+  {
+    for(int i=0;i<n.size;i++)
+    {
+      s << n.numbers[i] << "  ";
+    }
+    return s;
+  }
+  friend istream &operator >> (istream &s, const Numbers &n)
+  {
+    for (int i=0; i<n.size; i++)
+    {
+      s >> n.numbers[i];
+    }
+    return s;
+  }
 };
 
 #endif
--- 14.6/main.cpp
+++ 14.6/main.cpp
@@ -2,15 +2,18 @@
 #include <iostream>
 using namespace std;
 using namespace std;
-
+// Professor, I did it all over agian myself, I combined hpp and cpp together and I used different ways to give the values in the operators and set the numbers amount to 10. First compile it will show the random numbers from 0-99 and then you have to input values yourself and the result will show up. 
 int main()
 {
-  Numbers n1(5);
+  Numbers n1(10);
   
-  cout<<"Numbers:"<<endl<<n1;
-  cout<<"\n\nInsert new numbers: "<<endl;
-  cin>>n1;
-  cout<<"\nNew numbers:"<<endl<<n1;
+  cout << " please check the Numbers below : " << endl;
+  cout << n1 << endl;
+
+  cout << " add 10 new numbers " << endl;
+  cin >> n1;
+
+  cout << " New Numbers " << n1 << endl;
 
   return 0;
 }
