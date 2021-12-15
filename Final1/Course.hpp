8cd16b808718260d664798b56b9342323b1cf82b
Junzhe Zhao <89234341+JunzheCS2@users.noreply.github.com>
2021-12-15 06:01:58 +0000

    update final1

--- Final1/Course.hpp
+++ Final1/Course.hpp
@@ -3,3 +3,24 @@
 #include <iostream>
 using namespace std;
 
+class Course // create a course by class
+{
+  private:
+    int courseID;
+    string courseName;
+    int grade;
+  public:
+    Course()
+    {
+      courseID = 0;
+      courseName = " ";
+      grade = 0; // constructor
+    }
+
+    Course(int id, string name, int Grade)
+    {
+      courseID = id;
+      courseName = name;
+      grade = Grade; // paramaterized constructor
+    }
+}
