148822e347ba88cc93261551b149a65f2eb82277
Junzhe Zhao <89234341+JunzheCS2@users.noreply.github.com>
2021-12-15 21:53:49 +0000

    update final2

---
+++ final2/main.cpp
@@ -0,0 +1,50 @@
+#include "Course.hpp"
+void printCourses(Course *c, int size);
+void swapCourses(Course *c1, Course *c2);
+int partition(Course *c, int s, int e);
+void quickSort(Course *c, int s, int e);
+
+int main()
+{
+  // use the previous data
+  Course courses[10];
+  courses[0].setCourseID(10001111);
+  courses[0].setCourseName("James");
+  courses[0].setGrade(100);
+
+  courses[1].setCourseID(10009999);
+  courses[1].setCourseName("Brick");
+  courses[1].setGrade(80);
+
+  courses[2].setCourseID(10008888);
+  courses[2].setCourseName("Amy");
+  courses[2].setGrade(90);
+
+  courses[3].setCourseID(10007771);
+  courses[3].setCourseName("Bill");
+  courses[3].setGrade(100);
+
+  courses[4].setCourseID(10009900);
+  courses[4].setCourseName("John");
+  courses[4].setGrade(10);
+
+  courses[5].setCourseID(10006677);
+  courses[5].setCourseName("Kurt");
+  courses[5].setGrade(100);
+
+  courses[6].setCourseID(10007766);
+  courses[6].setCourseName("Joe");
+  courses[6].setCourseID(90);
+
+  courses[7].setCourseID(10007772);
+  courses[7].setCourseName("Steph");
+  courses[7].setGrade(90);
+
+  courses[8].setCourseID(10005566);
+  courses[8].setCourseName("Jack");
+  courses[8].setGrade(80);
+
+  courses[9].setCourseID(10007773);
+  courses[9].setCourseName("Zack");
+  courses[9].setGrade(90);
+}
