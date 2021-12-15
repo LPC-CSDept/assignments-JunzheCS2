9cdb0489319e68931a0ca6021dde3335b317fccf
Junzhe Zhao <89234341+JunzheCS2@users.noreply.github.com>
2021-12-15 07:00:51 +0000

    update final1

--- Final1/main.cpp
+++ Final1/main.cpp
@@ -4,38 +4,42 @@ int main()
 {
   Course courses[10];
   courses[0].setCourseID(10001111);
-  courses[0].setCourseID(10001111);
-  courses[0].setCourseID(10001111);
+  courses[0].setCourseName("James");
+  courses[0].setGrade(100);
 
-  courses[0].setCourseID(10001111);
-  courses[0].setCourseID(10001111);
-  courses[0].setCourseID(10001111);
+  courses[1].setCourseID(10009999);
+  courses[1].setCourseName("Brick");
+  courses[1].setGrade(80);
 
-  courses[0].setCourseID(10001111);
-  courses[0].setCourseID(10001111);
-  courses[0].setCourseID(10001111);
+  courses[2].setCourseID(10008888);
+  courses[2].setCourseName("Amy");
+  courses[2].setGrade(90);
 
-  courses[0].setCourseID(10001111);
-  courses[0].setCourseID(10001111);
-  courses[0].setCourseID(10001111);
+  courses[3].setCourseID(10007771);
+  courses[3].setCourseName("Bill");
+  courses[3].setGrade(100);
 
-  courses[0].setCourseID(10001111);
-  courses[0].setCourseID(10001111);
-  courses[0].setCourseID(10001111);
+  courses[4].setCourseID(10009900);
+  courses[4].setCourseName("John");
+  courses[4].setGrade(10);
 
-  courses[0].setCourseID(10001111);
-  courses[0].setCourseID(10001111);
-  courses[0].setCourseID(10001111);
+  courses[5].setCourseID(10006677);
+  courses[5].setCourseName("Kurt");
+  courses[5].setGrade(100);
 
-  courses[0].setCourseID(10001111);
-  courses[0].setCourseID(10001111);
-  courses[0].setCourseID(10001111);
+  courses[6].setCourseID(10007766);
+  courses[6].setCourseName("Joe");
+  courses[6].setCourseID(90);
 
-  courses[0].setCourseID(10001111);
-  courses[0].setCourseID(10001111);
-  courses[0].setCourseID(10001111);
+  courses[7].setCourseID(10007772);
+  courses[7].setCourseName("Steph");
+  courses[7].setGrade(90);
 
-  courses[0].setCourseID(10001111);
-  courses[0].setCourseID(10001111);
-  courses[0].setCourseID(10001111);
+  courses[8].setCourseID(10005566);
+  courses[8].setCourseName("Jack");
+  courses[8].setGrade(80);
+
+  courses[8].setCourseID(10007773);
+  courses[8].setCourseName("Zack");
+  courses[8].setGrade(90);
 }
